35202ca4477190a34d2e7da6c88d9c533d02fbff
Anastasia Dusak <61540676+k-a-il@users.noreply.github.com>
2025-09-08 18:27:40 +0200

    IaC: renamed plugins

--- localstack-core/localstack/utils/catalog/plugins.py
+++ localstack-core/localstack/utils/catalog/plugins.py
@@ -7,7 +7,7 @@ from localstack.utils.objects import singleton_factory
 @singleton_factory
 def get_aws_catalog() -> CatalogPlugin:
     plugin_manager = PluginManager(CatalogPlugin.namespace)
-    plugin_name = "aws_catalog_pro"
+    plugin_name = "aws-catalog-pro"
     if not plugin_manager.exists(plugin_name):
-        plugin_name = "aws_catalog"
+        plugin_name = "aws-catalog"
     return plugin_manager.load(plugin_name)
